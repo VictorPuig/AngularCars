02359d5a223415c19f4708f16977e8bff4778784
Guillem Cruz <gcquiroga@gmail.com>
2016-10-19 17:07:41 +0200

    Afegida docuementacio al servidor

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var mysql = require("mysql");
 
 var app = express();
 
+// Creem una conexio a la BDD
 var con = mysql.createConnection({
   host: '192.168.1.43',
   user: 'root',
@@ -10,32 +11,42 @@ var con = mysql.createConnection({
   database: 'cars'
 });
 
+// Ens conectem a la BDD
 con.connect(function(err) {
   if (err) {
-    console.error('error connecting: ' + err.stack);
+    // En cas d'error, mostrar per consola
+    console.error('Error conectant a MySql: ' + err.stack);
     return;
   }
-
-  console.log('connected as id ' + con.threadId);
+  // Mostrem per consola que la conexio a la BDD ha sigut satisfactoria
+  console.log('Conectat a MySql amb id: ' + con.threadId);
 });
 
+// Prepara el directori "app" per a recursos estatics
 app.use(express.static("app"));
 
+// Ruta root
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+// Ruta getCars retorna les dades de la crida de la BDD en JSON
 app.get("/getCars", function(req, res){
+  // Executa la consulta SQL
   con.query("SELECT * FROM car_table", function queryCb(err, rows) {
     if (err) {
-      console.log(err);
+      // En cas d'error, imprimirlo per consola i respondre un JSON
+      // amb una key "err" amb una descripcio
+      console.err(err);
       throw err;
     }
-    console.log("peticio");
+
+    // Retornem les dades de la BDD
     res.send(rows);
   });
 });
 
+// Iniciem el servidor
 app.listen(8080, function(){
   console.log("Servidor iniciat a localhost:8080");
 });
