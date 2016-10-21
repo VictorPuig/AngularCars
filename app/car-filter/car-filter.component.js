7b2181fe616f864b8252ac7eb04e4186cb1d1c64
Guillem Cruz <gcquiroga@gmail.com>
2016-10-21 16:15:33 +0200

    Filtre local borrat ja que ara es filtra al servidor

--- app/car-filter/car-filter.component.js
+++ app/car-filter/car-filter.component.js
@@ -57,32 +57,6 @@ angular.
             }
           });
         }, true);
-
-        //.keys (funcio que agafa un objecte (self.data.maker) i retorna una array amb totes les keys (Marcas))
-        //.every (funcio de arrays que pregunta a cada element i comprova que cada element sigui true, retorna true, en el cas contrari, retorna false)
-        //Funcio filterCars s'executa per cada cotxe comprovant si s'ha de mostrar o no
-        self.data.filterCars = function filterCars(el) {
-          var makerEmpty = Object
-            .keys(self.data.filter.maker)
-            .every(function (key) { return !self.data.filter.maker[key]; });
-          var colorEmpty = Object
-            .keys(self.data.filter.color)
-            .every(function (key) { return !self.data.filter.color[key]; });
-
-            //si cap está marcat, retorna true (Es mostten tots)
-          if (makerEmpty && colorEmpty)
-            return true;
-
-            //si algun dels fabricants no esta marcat i els colors estan marcats, filtra pels colors
-          if (makerEmpty && !colorEmpty)
-            return self.data.filter.color[el.color];
-
-            //si algun dels colors no esta marcat i els fabricants estan marcats, filtra pels fabricants
-          if (!makerEmpty && colorEmpty)
-            return self.data.filter.maker[el.maker];
-
-          return self.data.filter.maker[el.maker] && self.data.filter.color[el.color];
-        };
       }
     ]
   });
